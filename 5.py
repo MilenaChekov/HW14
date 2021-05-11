0fa131316ebdf2130a8184eac66880debd401107
MilenaChekov <60537367+MilenaChekov@users.noreply.github.com>
2021-05-11 10:19:57 +0300

    Add files via upload

--- 5.py
+++ 5.py
@@ -5,13 +5,13 @@ from sklearn.manifold import TSNE
 
 covid = pd.read_csv('human_coronavirus_aln_scores.tsv', sep = '\t', index_col = 0)
 covid = 5000 - covid
-model = TSNE(metric = 'precomputed')
+model = TSNE(perplexity=50, metric= 'precomputed')
 X = model.fit_transform(covid)
 print(covid)
 print(X)
-covid[['PC1', 'PC2']] = X[:, 1:2]
+covid[['TS1', 'TS2']] = X[:, :2]
 covid['type'] = ['HCoV-HKU1']*20 + ['MERS-CoV']*20 + ['SARS-CoV-2']*20+['HCoV-229E']*20+['HCoV-NL63']*20+['HCoV-OC43']*20+['SARS-CoV']*20
-sns.scatterplot(x = 'PC1', y = 'PC3', hue = 'type', data = covid)
-plt.savefig("5.png")
+sns.scatterplot(x='TS1', y='TS2', hue='type', data=covid)
+plt.savefig("5_50.png")
 plt.close()
 
